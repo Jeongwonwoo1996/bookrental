9b9b0364f12f9b99542258dce41c74c94e992a3e
Tae young Kim <tyk0707@gmail.com>
2025-08-12 17:42:21 +0900

    commit

--- bookrental/src/io/github/bookrentalteam/bookrental/Main.java
+++ bookrental/src/io/github/bookrentalteam/bookrental/Main.java
@@ -1,9 +1,8 @@
 package io.github.bookrentalteam.bookrental;
 
 public class Main {
-
 	public static void main(String[] args) {
 		System.out.println("Book Rental System started.");
+		System.out.println("2025-08-12");
 	}
-
 }
